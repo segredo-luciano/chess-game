94558a7ec36a88b4a868935ff7c451fef8950726
segredo-luciano <lucianosegredo20@gmail.com>
2023-09-10 20:20:33 -0300

    commit de seguranca

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -162,7 +162,8 @@ public class ChessMatch {
 	}
 	
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece) board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		if(capturedPiece != null) {
@@ -173,7 +174,8 @@ public class ChessMatch {
 	}
 	
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece piece = board.removePiece(target);
+		ChessPiece piece = (ChessPiece) board.removePiece(target);
+		piece.decreaseMoveCount();
 		board.placePiece(piece, source);
 		
 		if(capturedPiece != null) {
